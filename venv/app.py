bb1833377a10657ec80259c944e4e89b7952ca49
ESTON-kiam <engestonbrandonkiama@gmail.com>
2025-02-21 11:45:34 +0300

    update

--- venv/app.py
+++ venv/app.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+class Demo:
+    def __init__(self):
+        self.name="demo"
+        
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -8,7 +14,8 @@ def home():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template("user.html",content=name,x=42)
+    demo=Demo()
+    return render_template("user.html",content=name,x=42,demo=demo)
 
 if __name__ == '__main__':
     app.run(debug=True)
